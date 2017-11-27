86135b0645ad337fb0638b2a7cd3c3177a93d251
Juliana Fogg <julianafogg@gmail.com>
2017-11-27 18:01:19 +0000

    automatically backed up by learn

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,9 +2,7 @@
 # the described class (or method) for the tests to even run. It's sort of a
 # freebie test to make sure you've actually defined the models you plan on
 # testing.
- cd my/cool/ruby/project
-rubocop
-
+ 
 describe Person do
 
   let(:avi_attributes) {
